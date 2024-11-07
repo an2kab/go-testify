0bb5cdc7ca9db7028cf1fbfa0e1090cc87eb36e0
Andrey Kabanov <an2kab@gmail.com>
2024-11-07 21:35:48 +0300

    second commit

--- precode.go
+++ precode.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"net/http"
-	"net/http/httptest"
 	"strconv"
 	"strings"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 var cafeList = map[string][]string{
@@ -47,52 +42,4 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(answer))
-}
-
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=6&city=moscow", nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	body := responseRecorder.Body.String()
-	list := strings.Split(body, ",")
-	assert.Lenf(t, totalCount, len(list), "expected cafe count %d, got %d", totalCount, len(list))
-}
-
-func TestMainHandlerWhenRequestCorrect(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=4&city=moscow", nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	// Проверка кода ответа
-	expected := http.StatusOK
-	result := responseRecorder.Code
-	require.Equal(t, expected, result, "status code must be 200")
-
-	// Проверка тела ответа
-	body := responseRecorder.Body.String()
-	require.NotEmpty(t, body, "body must be not empty")
-}
-
-func TestMainHandlerWhenNoCorrectCity(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=4&city=lipetsk", nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	// Проверка города
-	expected := http.StatusBadRequest
-	result := responseRecorder.Code
-	require.Equal(t, expected, result, "status code must be 400")
-
-	// Проверка тела ответа
-	body := responseRecorder.Body.String()
-	assert.Equal(t, "wrong city value", body)
-
 }
